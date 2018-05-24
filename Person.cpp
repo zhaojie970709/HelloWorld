aea5dbd7595475a4e58ff612d59405e2064bd127
zhaojie970709 <39575436+zhaojie970709@users.noreply.github.com>
2018-05-24 11:16:50 +0800

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,1 +1,20 @@
-
+const string& string::operator + (const string& a ,const string& b)
+{
+  len = a.len + b.len;
+  rep = new char[len+1];
+  strcpy(rep,a);
+  strcat(rep,b);
+  delete rep;
+  rep = nullptr;
+  return *this;
+}
+const string& string::operator += (const string& a)
+{
+  len += a.len;
+  char* temp = rep;
+  rep = new char[len+1];
+  strcpy(rep,temp);
+  strcat(rep,a);
+  delete temp;
+  return *this;
+}
